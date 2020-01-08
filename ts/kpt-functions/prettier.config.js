GoogleContainerTools/kpt-functions-sdk

commit eca651e0c87c634c44c6b51f289df08df8ef4bcd

kpt-functions framework uses gts style (#13)
* kpt-functions framework uses gts style
* Convert prettier config to ES6 and remove extra rules

ts/kpt-functions/prettier.config.js
@@ -1,0 +1,2 @@
+export const singleQuote = true;
+export const trailingComma = 'es5';

ts/kpt-functions/src/errors.ts
@@ -33,7 +33,7 @@ export class ConfigError extends Error {
  * Wraps multiple ConfigError objects.
  */
 export class MultiConfigError extends ConfigError {
-  public readonly errors: ConfigError[];
+  readonly errors: ConfigError[];
 
   constructor(message: string, errors: ConfigError[]) {
     super(message);
@@ -58,7 +58,7 @@ export class MultiConfigError extends ConfigError {
  * Represents an error with a configuration file.
  */
 export class ConfigFileError extends ConfigError {
-  constructor(message: string, public readonly path: string) {
+  constructor(message: string, readonly path: string) {
     super(message);
     this.name = 'ConfigFileError';
   }
@@ -72,7 +72,7 @@ export class ConfigFileError extends ConfigError {
  * Represents an error with a KubernetesObject.
  */
 export class KubernetesObjectError extends ConfigError {
-  constructor(message: string, public readonly object: KubernetesObject) {
+  constructor(message: string, readonly object: KubernetesObject) {
     super(message);
     this.name = 'KubernetesObjectError';
   }

ts/kpt-functions/src/io.ts
@@ -48,7 +48,7 @@ const YAML_STYLE: DumpOptions = {
 export function readConfigs(
   input: FilePath,
   format: FileFormat,
-  functionConfig?: FilePath | KubernetesObject,
+  functionConfig?: FilePath | KubernetesObject
 ): Configs {
   let inputRaw: string;
 
@@ -60,6 +60,11 @@ export function readConfigs(
       if (process.stdin.isTTY) {
         throw new Error('Cannot read input. Need either stdin or --input file');
       }
+      if (!fs.existsSync(input)) {
+        throw new Error(`Input file does not exist: ${input}`);
+      }
+      inputRaw = rw.readFileSync(input, 'utf8');
+      break;
     default:
       if (!fs.existsSync(input)) {
         throw new Error(`Input file does not exist: ${input}`);
@@ -90,9 +95,9 @@ export function readConfigs(
 export function parse(
   input: string,
   format: FileFormat,
-  functionConfig?: string | KubernetesObject,
+  functionConfig?: string | KubernetesObject
 ): Configs {
-  let i = load(input, format);
+  const i = load(input, format);
   let f;
   if (typeof functionConfig === 'string') {
     f = load(functionConfig, format);
@@ -124,7 +129,7 @@ function load(raw: string, format: FileFormat): any {
  * @param format defines whether to write the Configs as YAML or JSON.
  */
 export function writeConfigs(output: FilePath, configs: Configs, format: FileFormat): void {
-  if (output == '/dev/null') {
+  if (output === '/dev/null') {
     return;
   }
 

ts/kpt-functions/src/metadata.ts
@@ -31,7 +31,7 @@ export const SOURCE_INDEX_ANNOTATION = `${ANNOTATION_PREFIX}/index`;
 export function addAnnotation(
   o: KubernetesObject,
   annotation: string,
-  value: string,
+  value: string
 ): KubernetesObject {
   o.metadata.annotations = addToObject(o.metadata.annotations, annotation, value);
   return o;
@@ -103,7 +103,7 @@ export function getLabel(o: KubernetesObject, label: string): string | undefined
 function addToObject(
   object: { [key: string]: string } | undefined,
   key: string,
-  value: string,
+  value: string
 ): { [key: string]: string } {
   return Object.assign(object || {}, { [key]: value });
 }
@@ -116,7 +116,7 @@ function removeFromObject(object: { [key: string]: string } | undefined, key: st
 
 function getFromObject(
   object: { [key: string]: string } | undefined,
-  key: string,
+  key: string
 ): string | undefined {
   return object && object[key];
 }

ts/kpt-functions/src/run.ts
@@ -98,12 +98,12 @@ Use this ONLY if the function accepts a ConfigMap.`,
   });
 
   // Parse args.
-  let args = new Map(Object.entries(parser.parseArgs()));
-  let fileFormat = Boolean(args.get('json')) ? FileFormat.JSON : FileFormat.YAML;
-  let inputFile = args.get('input') || '/dev/stdin';
-  let outputFile = args.get('output') || '/dev/stdout';
+  const args = new Map(Object.entries(parser.parseArgs()));
+  const fileFormat = Boolean(args.get('json')) ? FileFormat.JSON : FileFormat.YAML;
+  const inputFile = args.get('input') || '/dev/stdin';
+  const outputFile = args.get('output') || '/dev/stdout';
   let functionConfig: string | KubernetesObject | undefined = args.get('function_config');
-  let functionConfigLiterals = args.get('function_config_literal');
+  const functionConfigLiterals = args.get('function_config_literal');
   if (functionConfigLiterals) {
     if (functionConfig) {
       parser.error('--function-config and --function-config-literal are mutually exclusive');
@@ -129,10 +129,10 @@ function runFn(
   inputFile: string,
   outputFile: string,
   fileFormat: FileFormat,
-  functionConfig?: string | KubernetesObject,
+  functionConfig?: string | KubernetesObject
 ) {
   // Parse Config.
-  let configs = readConfigs(inputFile, fileFormat, functionConfig);
+  const configs = readConfigs(inputFile, fileFormat, functionConfig);
 
   // Run the function.
   const err = fn(configs);
@@ -145,7 +145,7 @@ function runFn(
 }
 
 function parseToConfigMap(parser: ArgumentParser, args: string[][]): KubernetesObject {
-  let cm: any = {
+  const cm: any = {
     apiVersion: 'v1',
     kind: 'ConfigMap',
     metadata: {

ts/kpt-functions/src/testing.ts
@@ -39,7 +39,7 @@ export class TestRunner {
   run(
     input: Configs = new Configs(),
     expectedOutput?: Configs | ConfigError,
-    expectException?: boolean,
+    expectException?: boolean
   ): () => void {
     return new TestCase(this.fn, input, expectedOutput, expectException).run();
   }
@@ -53,10 +53,10 @@ class TestCase {
     private readonly fn: KptFunc,
     private readonly input: Configs = new Configs(),
     private readonly expectedOutput?: Configs | ConfigError,
-    private readonly expectException?: boolean,
+    private readonly expectException?: boolean
   ) {}
 
-  public run(): () => void {
+  run(): () => void {
     return () => {
       // We must clone the input as runner.fn may mutate its input Configs.
       const configs = deepClone(this.input);

ts/kpt-functions/src/types.ts
@@ -75,7 +75,7 @@ export class Configs {
    *
    * Returned objects are pass-by-reference; mutating them results in changes being persisted.
    */
-  public getAll(): KubernetesObject[] {
+  getAll(): KubernetesObject[] {
     return this.items.map((e) => e[1]);
   }
 
@@ -90,7 +90,7 @@ export class Configs {
    *
    * @param isKind is a type predicate on the desired type.
    */
-  public get<Kind extends KubernetesObject>(isKind: (o: KubernetesObject) => o is Kind): Kind[] {
+  get<Kind extends KubernetesObject>(isKind: (o: KubernetesObject) => o is Kind): Kind[] {
     return this.getAll().filter(isKind) as Kind[];
   }
 
@@ -106,7 +106,7 @@ export class Configs {
    *
    * @param objects The objects to insert.
    */
-  public insert(...objects: KubernetesObject[]): void {
+  insert(...objects: KubernetesObject[]): void {
     objects.forEach((o) => {
       const key: string = kubernetesKeyFn(o);
       const [index, found] = this.indexOf(key, this.items, 0);
@@ -121,7 +121,7 @@ export class Configs {
    *
    * @param objects The objects to delete.
    */
-  public delete(...objects: KubernetesObject[]): void {
+  delete(...objects: KubernetesObject[]): void {
     objects.forEach((o) => {
       const key: string = kubernetesKeyFn(o);
       const [index, found] = this.indexOf(key, this.items, 0);
@@ -134,7 +134,7 @@ export class Configs {
   /**
    * Deletes all objects.
    */
-  public deleteAll(): void {
+  deleteAll(): void {
     this.items = [];
   }
 
@@ -149,7 +149,7 @@ export class Configs {
    *
    * @param keyFn Generates a key for each Value.
    */
-  public groupBy(keyFn: (object: KubernetesObject) => string): Array<[string, KubernetesObject[]]> {
+  groupBy(keyFn: (object: KubernetesObject) => string): Array<[string, KubernetesObject[]]> {
     const map = new Map<string, KubernetesObject[]>();
     this.getAll().forEach((o) => {
       const key = keyFn(o);
@@ -162,7 +162,7 @@ export class Configs {
   /**
    * Returns the functionConfig if defined.
    */
-  public getFunctionConfig(): KubernetesObject | undefined {
+  getFunctionConfig(): KubernetesObject | undefined {
     return this.functionConfig;
   }
 
@@ -176,7 +176,7 @@ export class Configs {
    *
    * @key key The key in the 'data' field in the ConfigMap object passed as the functionConfig.
    */
-  public getFunctionConfigValue(key: string): string | undefined {
+  getFunctionConfigValue(key: string): string | undefined {
     const cm = this.functionConfig;
     if (!cm) {
       return undefined;
@@ -190,7 +190,7 @@ export class Configs {
   /**
    * Similar to {@link getFunctionConfigValue} except it throws an exception if the given key is undefined.
    */
-  public getFunctionConfigValueOrThrow(key: string): string {
+  getFunctionConfigValueOrThrow(key: string): string {
     const val = this.getFunctionConfigValue(key);
     if (val === undefined) {
       throw new Error(`Missing key ${key} in ConfigMap data provided as functionConfig`);
@@ -208,7 +208,7 @@ export class Configs {
   private indexOf(
     key: string,
     array: Array<[string, KubernetesObject]>,
-    offset: number,
+    offset: number
   ): [number, boolean] {
     if (array.length === 0) {
       // array is empty so the value cannot be in it.

ts/kpt-functions/src/types_test.ts
@@ -19,11 +19,11 @@ import { ObjectMeta } from './gen/io.k8s.apimachinery.pkg.apis.meta.v1';
 import { Configs, KubernetesObject } from './types';
 
 class Role implements KubernetesObject {
-  public readonly apiVersion: string = 'rbac.authorization.k8s.io/v1';
-  public readonly kind: string = 'Role';
-  public metadata: ObjectMeta;
+  readonly apiVersion: string = 'rbac.authorization.k8s.io/v1';
+  readonly kind: string = 'Role';
+  metadata: ObjectMeta;
 
-  public roleField: string = '';
+  roleField = '';
 
   constructor(name: string, roleField?: string) {
     if (roleField) {
@@ -33,23 +33,23 @@ class Role implements KubernetesObject {
   }
 }
 
-function isRole(o: any): o is Role {
+function isRole(o: KubernetesObject): o is Role {
   return o.apiVersion === 'rbac.authorization.k8s.io/v1' && o.kind === 'Role';
 }
 
 class Pod implements KubernetesObject {
-  public readonly apiVersion: string = 'v1';
-  public readonly kind: string = 'Pod';
-  public metadata: ObjectMeta;
+  readonly apiVersion: string = 'v1';
+  readonly kind: string = 'Pod';
+  metadata: ObjectMeta;
 
-  public roleBindingField: string = '';
+  roleBindingField = '';
 
   constructor(name: string) {
     this.metadata = { name };
   }
 }
 
-function isPod(o: any): o is Pod {
+function isPod(o: KubernetesObject): o is Pod {
   return o.apiVersion === 'v1' && o.kind === 'Pod';
 }
 

commit c31240440854c867d2bb0730cde01cd8f92af0ee

Fix linter.
It was broken by PR #13.

ts/kpt-functions/prettier.config.js
@@ -1,2 +1,0 @@
-export const singleQuote = true;
-export const trailingComma = 'es5';

ts/kpt-functions/src/errors.ts
@@ -78,7 +78,9 @@ export class KubernetesObjectError extends ConfigError {
   }
 
   toString(): string {
-    const path = getAnnotation(this.object, SOURCE_PATH_ANNOTATION) || 'No path annotation';
+    const path =
+      getAnnotation(this.object, SOURCE_PATH_ANNOTATION) ||
+      'No path annotation';
     const namespace = this.object.metadata.namespace || '';
     return `${this.name}: ${this.message}
 path: ${path}

ts/kpt-functions/src/io.ts
@@ -128,7 +128,11 @@ function load(raw: string, format: FileFormat): any {
  * @param configs Contains objects to write to the output file.
  * @param format defines whether to write the Configs as YAML or JSON.
  */
-export function writeConfigs(output: FilePath, configs: Configs, format: FileFormat): void {
+export function writeConfigs(
+  output: FilePath,
+  configs: Configs,
+  format: FileFormat
+): void {
   if (output === '/dev/null') {
     return;
   }

ts/kpt-functions/src/metadata.ts
@@ -33,7 +33,11 @@ export function addAnnotation(
   annotation: string,
   value: string
 ): KubernetesObject {
-  o.metadata.annotations = addToObject(o.metadata.annotations, annotation, value);
+  o.metadata.annotations = addToObject(
+    o.metadata.annotations,
+    annotation,
+    value
+  );
   return o;
 }
 
@@ -44,9 +48,16 @@ export function addAnnotation(
  * @param o The object to remove the annotation from.
  * @param annotation The annotation to remove.
  */
-export function removeAnnotation(o: KubernetesObject, annotation: string): KubernetesObject {
+export function removeAnnotation(
+  o: KubernetesObject,
+  annotation: string
+): KubernetesObject {
   removeFromObject(o.metadata.annotations, annotation);
-  if (o.metadata && o.metadata.annotations && Object.keys(o.metadata.annotations).length === 0) {
+  if (
+    o.metadata &&
+    o.metadata.annotations &&
+    Object.keys(o.metadata.annotations).length === 0
+  ) {
     delete o.metadata.annotations;
   }
   return o;
@@ -58,7 +69,10 @@ export function removeAnnotation(o: KubernetesObject, annotation: string): Kuber
  * @param o The object to get the annotation from.
  * @param annotation The annotation to get.
  */
-export function getAnnotation(o: KubernetesObject, annotation: string): string | undefined {
+export function getAnnotation(
+  o: KubernetesObject,
+  annotation: string
+): string | undefined {
   return getFromObject(o.metadata.annotations, annotation);
 }
 
@@ -70,7 +84,11 @@ export function getAnnotation(o: KubernetesObject, annotation: string): string |
  * @param label The label to set.
  * @param value The value to set the label to.
  */
-export function addLabel(o: KubernetesObject, label: string, value: string): KubernetesObject {
+export function addLabel(
+  o: KubernetesObject,
+  label: string,
+  value: string
+): KubernetesObject {
   o.metadata.labels = addToObject(o.metadata.labels, label, value);
   return o;
 }
@@ -82,9 +100,16 @@ export function addLabel(o: KubernetesObject, label: string, value: string): Kub
  * @param o The object to remove the label from.
  * @param label The label to remove.
  */
-export function removeLabel(o: KubernetesObject, label: string): KubernetesObject {
+export function removeLabel(
+  o: KubernetesObject,
+  label: string
+): KubernetesObject {
   removeFromObject(o.metadata.labels, label);
-  if (o.metadata && o.metadata.labels && Object.keys(o.metadata.labels).length === 0) {
+  if (
+    o.metadata &&
+    o.metadata.labels &&
+    Object.keys(o.metadata.labels).length === 0
+  ) {
     delete o.metadata.labels;
   }
   return o;
@@ -96,7 +121,10 @@ export function removeLabel(o: KubernetesObject, label: string): KubernetesObjec
  * @param o The object to get the label from.
  * @param label The label to get.
  */
-export function getLabel(o: KubernetesObject, label: string): string | undefined {
+export function getLabel(
+  o: KubernetesObject,
+  label: string
+): string | undefined {
   return getFromObject(o.metadata.labels, label);
 }
 
@@ -108,7 +136,10 @@ function addToObject(
   return Object.assign(object || {}, { [key]: value });
 }
 
-function removeFromObject(object: { [key: string]: string } | undefined, key: string) {
+function removeFromObject(
+  object: { [key: string]: string } | undefined,
+  key: string
+) {
   if (object && object[key]) {
     delete object[key];
   }

ts/kpt-functions/src/run.ts
@@ -98,14 +98,20 @@ Use this ONLY if the function accepts a ConfigMap.`,
 
   // Parse args.
   const args = new Map(Object.entries(parser.parseArgs()));
-  const fileFormat = Boolean(args.get('json')) ? FileFormat.JSON : FileFormat.YAML;
+  const fileFormat = Boolean(args.get('json'))
+    ? FileFormat.JSON
+    : FileFormat.YAML;
   const inputFile = args.get('input') || '/dev/stdin';
   const outputFile = args.get('output') || '/dev/stdout';
-  let functionConfig: string | KubernetesObject | undefined = args.get('function_config');
+  let functionConfig: string | KubernetesObject | undefined = args.get(
+    'function_config'
+  );
   const functionConfigLiterals = args.get('function_config_literal');
   if (functionConfigLiterals) {
     if (functionConfig) {
-      parser.error('--function-config and --function-config-literal are mutually exclusive');
+      parser.error(
+        '--function-config and --function-config-literal are mutually exclusive'
+      );
     }
     functionConfig = parseToConfigMap(parser, functionConfigLiterals);
   }
@@ -143,7 +149,10 @@ function runFn(
   writeConfigs(outputFile, configs, fileFormat);
 }
 
-function parseToConfigMap(parser: ArgumentParser, args: string[][]): KubernetesObject {
+function parseToConfigMap(
+  parser: ArgumentParser,
+  args: string[][]
+): KubernetesObject {
   const cm: any = {
     apiVersion: 'v1',
     kind: 'ConfigMap',
@@ -154,7 +163,9 @@ function parseToConfigMap(parser: ArgumentParser, args: string[][]): KubernetesO
   };
   for (const a of args) {
     if (a.length !== 1) {
-      parser.error('Exactly one value is required for --function-config-literal');
+      parser.error(
+        'Exactly one value is required for --function-config-literal'
+      );
     }
     const kv = a[0].split('=');
     if (kv.length !== 2) {

ts/kpt-functions/src/testing.ts
@@ -95,7 +95,9 @@ class TestCase {
       //  We know the configs are sorted, but not elements of sub-fields which are arrays.
       //  The comparison doesn't try to smartly find plausible near-misses in the case of missing
       //  elements, so missing the first element of 8 will throw a large, hard-to-read error message.
-      expect(valueOf(configs)).toEqual(valueOf(this.expectedOutput) || valueOf(this.input));
+      expect(valueOf(configs)).toEqual(
+        valueOf(this.expectedOutput) || valueOf(this.input)
+      );
     };
   }
 }
@@ -103,7 +105,8 @@ class TestCase {
 function deepClone(configs: Configs): Configs {
   const items = JSON.parse(JSON.stringify(configs.getAll()));
   const functionConfig =
-    configs.getFunctionConfig() && JSON.parse(JSON.stringify(configs.getFunctionConfig()));
+    configs.getFunctionConfig() &&
+    JSON.parse(JSON.stringify(configs.getFunctionConfig()));
   return new Configs(items, functionConfig);
 }
 

ts/kpt-functions/src/types.ts
@@ -52,7 +52,10 @@ export class Configs {
    *
    * Does not preserve insertion order of the passed objects.
    */
-  constructor(items: KubernetesObject[] = [], functionConfig?: KubernetesObject) {
+  constructor(
+    items: KubernetesObject[] = [],
+    functionConfig?: KubernetesObject
+  ) {
     this.functionConfig = functionConfig;
     this.insert(...items);
   }
@@ -65,7 +68,7 @@ export class Configs {
    * Returned objects are pass-by-reference; mutating them results in changes being persisted.
    */
   getAll(): KubernetesObject[] {
-    return this.items.map((e) => e[1]);
+    return this.items.map(e => e[1]);
   }
 
   /**
@@ -79,7 +82,9 @@ export class Configs {
    *
    * @param isKind is a type predicate on the desired type.
    */
-  get<Kind extends KubernetesObject>(isKind: (o: KubernetesObject) => o is Kind): Kind[] {
+  get<Kind extends KubernetesObject>(
+    isKind: (o: KubernetesObject) => o is Kind
+  ): Kind[] {
     return this.getAll().filter(isKind) as Kind[];
   }
 
@@ -96,7 +101,7 @@ export class Configs {
    * @param objects The objects to insert.
    */
   insert(...objects: KubernetesObject[]): void {
-    objects.forEach((o) => {
+    objects.forEach(o => {
       const key: string = kubernetesKeyFn(o);
       const [index, found] = this.indexOf(key, this.items, 0);
       this.items.splice(index, found ? 1 : 0, [key, o]);
@@ -111,7 +116,7 @@ export class Configs {
    * @param objects The objects to delete.
    */
   delete(...objects: KubernetesObject[]): void {
-    objects.forEach((o) => {
+    objects.forEach(o => {
       const key: string = kubernetesKeyFn(o);
       const [index, found] = this.indexOf(key, this.items, 0);
       if (found) {
@@ -138,9 +143,11 @@ export class Configs {
    *
    * @param keyFn Generates a key for each Value.
    */
-  groupBy(keyFn: (object: KubernetesObject) => string): Array<[string, KubernetesObject[]]> {
+  groupBy(
+    keyFn: (object: KubernetesObject) => string
+  ): Array<[string, KubernetesObject[]]> {
     const map = new Map<string, KubernetesObject[]>();
-    this.getAll().forEach((o) => {
+    this.getAll().forEach(o => {
       const key = keyFn(o);
       const valuesAtKey = map.get(key) || [];
       map.set(key, [...valuesAtKey, o]);
@@ -171,7 +178,9 @@ export class Configs {
       return undefined;
     }
     if (!isConfigMap(cm)) {
-      throw new Error(`functionConfig expected to be of kind ConfigMap, instead got: ${cm.kind}`);
+      throw new Error(
+        `functionConfig expected to be of kind ConfigMap, instead got: ${cm.kind}`
+      );
     }
     return cm.data && cm.data[key];
   }
@@ -182,7 +191,9 @@ export class Configs {
   getFunctionConfigValueOrThrow(key: string): string {
     const val = this.getFunctionConfigValue(key);
     if (val === undefined) {
-      throw new Error(`Missing key ${key} in ConfigMap data provided as functionConfig`);
+      throw new Error(
+        `Missing key ${key} in ConfigMap data provided as functionConfig`
+      );
     }
     return val;
   }
@@ -210,7 +221,11 @@ export class Configs {
       return this.indexOf(key, array.slice(0, mid), offset);
     } else if (key > array[mid][0]) {
       // Look after mid.
-      return this.indexOf(key, array.slice(mid + 1, array.length), offset + mid + 1);
+      return this.indexOf(
+        key,
+        array.slice(mid + 1, array.length),
+        offset + mid + 1
+      );
     }
     // mid is the object we're looking for.
     return [offset + mid, true];
@@ -240,7 +255,13 @@ export interface KubernetesObject {
  * Type guard for KubernetesObject.
  */
 export function isKubernetesObject(o: any): o is KubernetesObject {
-  return o && o.apiVersion !== '' && o.kind !== '' && o.metadata && o.metadata.name !== '';
+  return (
+    o &&
+    o.apiVersion !== '' &&
+    o.kind !== '' &&
+    o.metadata &&
+    o.metadata.name !== ''
+  );
 }
 
 /**

ts/kpt-functions/src/types_test.ts
@@ -114,12 +114,12 @@ describe('get', () => {
       'tinek',
       'walherich',
     ];
-    const roles = names.map((name) => new Role(name));
+    const roles = names.map(name => new Role(name));
     _.shuffle(roles);
 
     const configs = new Configs(roles);
 
-    expect(configs.getAll()).toEqual(names.map((name) => new Role(name)));
+    expect(configs.getAll()).toEqual(names.map(name => new Role(name)));
   });
 });
 
@@ -154,13 +154,13 @@ describe('insert', () => {
       'theo',
       'zuzka',
     ];
-    const roles = names.map((name) => new Role(name));
+    const roles = names.map(name => new Role(name));
     _.shuffle(roles);
 
     const configs = new Configs();
     configs.insert(...roles);
 
-    expect(configs.getAll()).toEqual(names.map((name) => new Role(name)));
+    expect(configs.getAll()).toEqual(names.map(name => new Role(name)));
   });
 });
 
@@ -220,7 +220,10 @@ describe('groupBy', () => {
     const configs = new Configs([new Role('alice'), new Role('bob')]);
     const grouped = configs.groupBy(keyFn);
 
-    expect(grouped).toEqual([['a', [new Role('alice')]], ['b', [new Role('bob')]]]);
+    expect(grouped).toEqual([
+      ['a', [new Role('alice')]],
+      ['b', [new Role('bob')]],
+    ]);
   });
 
   it('uses a single partition for elements mapping to the same key', () => {