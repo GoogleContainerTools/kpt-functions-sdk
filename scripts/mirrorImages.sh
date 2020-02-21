3195861d491fd2014392393fb9943eed3c91282b
Prachi Pendse <prachirp@google.com>
2020-02-20 17:37:09 -0800

    Address review comments

--- scripts/mirrorImages.sh
+++ scripts/mirrorImages.sh
@@ -2,20 +2,16 @@
 
 set -euo pipefail
 
-# Declare source image project id
-declare projectid=$1
-shift
-
-# Declare and populate array of source and destination image names
 declare -A imagearray=()
-for arg; do
-  eval `echo "imagearray$arg"`
-done
+imagearray["gcr.io/kpt-functions/read-yaml"]="gcr.io/config-management-release/read-yaml"
+imagearray["gcr.io/kpt-functions/write-yaml"]="gcr.io/config-management-release/write-yaml"
+imagearray["gcr.io/kpt-functions/gatekeeper-validate"]="gcr.io/config-management-release/policy-controller-validate"
+imagearray["gcr.io/kpt-dev/kpt"]="gcr.io/config-management-release/kpt"
 
-for image in ${!imagearray[@]}; do
-  docker pull gcr.io/${projectid}/${image}:latest
-  docker tag gcr.io/${projectid}/${image}:latest gcr.io/config-management-release/${imagearray[$image]}:latest
-  docker tag gcr.io/${projectid}/${image}:latest gcr.io/config-management-release/${imagearray[$image]}:stable
-  docker push gcr.io/config-management-release/${imagearray[$image]}:latest
-  docker push gcr.io/config-management-release/${imagearray[$image]}:stable
+for image in "${!imagearray[@]}"; do
+  docker pull ${image}
+  docker tag ${image} ${imagearray[$image]}:latest
+  docker tag ${image} ${imagearray[$image]}:stable
+  docker push ${imagearray[$image]}:latest
+  docker push ${imagearray[$image]}:stable
 done
