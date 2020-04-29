936453c784fd03572d41fde9ff8a3c50e481256e
Prachi Pendse <prachirp@google.com>
2020-04-28 19:11:17 -0700

    Convert from godep to gomod (#140)
    * Set GO111MODULE to fix go-ci

--- go/vendor/github.com/open-policy-agent/gatekeeper/pkg/target/target.go
+++ go/vendor/github.com/open-policy-agent/gatekeeper/pkg/target/target.go
@@ -11,12 +11,10 @@ import (
 	"github.com/open-policy-agent/frameworks/constraint/pkg/types"
 	"github.com/pkg/errors"
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/validation"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
@@ -38,25 +36,6 @@ type WipeData struct{}
 
 func processWipeData() (bool, string, interface{}, error) {
 	return true, "", nil, nil
-}
-
-type AugmentedReview struct {
-	AdmissionRequest *admissionv1beta1.AdmissionRequest
-	Namespace        *corev1.Namespace
-}
-
-type gkReview struct {
-	*admissionv1beta1.AdmissionRequest
-	Unstable *unstable `json:"_unstable,omitempty"`
-}
-
-type AugmentedUnstructured struct {
-	Object    unstructured.Unstructured
-	Namespace *corev1.Namespace
-}
-
-type unstable struct {
-	Namespace *corev1.Namespace `json:"namespace,omitempty"`
 }
 
 func processUnstructured(o *unstructured.Unstructured) (bool, string, interface{}, error) {
@@ -94,72 +73,8 @@ func (h *K8sValidationTarget) HandleReview(obj interface{}) (bool, interface{}, 
 		return true, data, nil
 	case *admissionv1beta1.AdmissionRequest:
 		return true, data, nil
-	case AugmentedReview:
-		return true, &gkReview{AdmissionRequest: data.AdmissionRequest, Unstable: &unstable{Namespace: data.Namespace}}, nil
-	case *AugmentedReview:
-		return true, &gkReview{AdmissionRequest: data.AdmissionRequest, Unstable: &unstable{Namespace: data.Namespace}}, nil
-	case AugmentedUnstructured:
-		admissionRequest, err := augmentedUnstructuredToAdmissionRequest(data)
-		if err != nil {
-			return false, nil, err
-		}
-		return true, admissionRequest, nil
-	case *AugmentedUnstructured:
-		admissionRequest, err := augmentedUnstructuredToAdmissionRequest(*data)
-		if err != nil {
-			return false, nil, err
-		}
-		return true, admissionRequest, nil
-	case unstructured.Unstructured:
-		admissionRequest, err := unstructuredToAdmissionRequest(data)
-		if err != nil {
-			return false, nil, err
-		}
-		return true, admissionRequest, nil
-	case *unstructured.Unstructured:
-		admissionRequest, err := unstructuredToAdmissionRequest(*data)
-		if err != nil {
-			return false, nil, err
-		}
-		return true, admissionRequest, nil
 	}
 	return false, nil, nil
-}
-
-func augmentedUnstructuredToAdmissionRequest(obj AugmentedUnstructured) (gkReview, error) {
-	req, err := unstructuredToAdmissionRequest(obj.Object)
-	if err != nil {
-		return gkReview{}, err
-	}
-
-	review := gkReview{AdmissionRequest: &req, Unstable: &unstable{Namespace: obj.Namespace}}
-
-	if obj.Namespace != nil {
-		review.Namespace = obj.Namespace.Name
-	}
-
-	return review, nil
-}
-
-func unstructuredToAdmissionRequest(obj unstructured.Unstructured) (admissionv1beta1.AdmissionRequest, error) {
-	resourceJSON, err := json.Marshal(obj.Object)
-	if err != nil {
-		return admissionv1beta1.AdmissionRequest{}, errors.New("Unable to marshal JSON encoding of object")
-	}
-
-	req := admissionv1beta1.AdmissionRequest{
-		Kind: metav1.GroupVersionKind{
-			Group:   obj.GetObjectKind().GroupVersionKind().Group,
-			Version: obj.GetObjectKind().GroupVersionKind().Version,
-			Kind:    obj.GetObjectKind().GroupVersionKind().Kind,
-		},
-		Object: runtime.RawExtension{
-			Raw: resourceJSON,
-		},
-		Name: obj.GetName(),
-	}
-
-	return req, nil
 }
 
 func getString(m map[string]interface{}, k string) (string, error) {
@@ -175,19 +90,6 @@ func getString(m map[string]interface{}, k string) (string, error) {
 		return "", fmt.Errorf("review[kind][%s] is not a string: %+v", k, val)
 	}
 	return s, nil
-}
-
-// nestedMap augments unstructured.NestedMap to interpret a nil-valued field
-// as missing
-func nestedMap(rmap map[string]interface{}, field string) (map[string]interface{}, bool, error) {
-	objMap, found, err := unstructured.NestedMap(rmap, field)
-	if err != nil || !found {
-		if val, found, err2 := unstructured.NestedFieldNoCopy(rmap, field); val == nil && found && err2 == nil {
-			return nil, false, nil
-		}
-		return nil, false, err
-	}
-	return objMap, true, nil
 }
 
 func (h *K8sValidationTarget) HandleViolation(result *types.Result) error {
@@ -214,20 +116,13 @@ func (h *K8sValidationTarget) HandleViolation(result *types.Result) error {
 		apiVersion = fmt.Sprintf("%s/%s", group, version)
 	}
 
-	objMap, found, err := nestedMap(rmap, "object")
+	objMap, found, err := unstructured.NestedMap(rmap, "object")
 	if err != nil {
 		return errors.Wrap(err, "HandleViolation:NestedMap")
 	}
 	if !found {
-		objMap, found, err = nestedMap(rmap, "oldObject")
-		if err != nil {
-			return errors.Wrap(err, "HandleViolation:NestedMapOldObj")
-		}
-		if !found {
-			return errors.New("no object or oldObject returned in review")
-		}
+		return errors.New("no object returned in review")
 	}
-
 	objMap["apiVersion"] = apiVersion
 	objMap["kind"] = kind
 
@@ -296,10 +191,6 @@ func (h *K8sValidationTarget) MatchSchema() apiextensions.JSONSchemaProps {
 				},
 			},
 			"namespaces": apiextensions.JSONSchemaProps{
-				Type: "array",
-				Items: &apiextensions.JSONSchemaPropsOrArray{
-					Schema: &apiextensions.JSONSchemaProps{Type: "string"}}},
-			"excludedNamespaces": apiextensions.JSONSchemaProps{
 				Type: "array",
 				Items: &apiextensions.JSONSchemaPropsOrArray{
 					Schema: &apiextensions.JSONSchemaProps{Type: "string"}}},
