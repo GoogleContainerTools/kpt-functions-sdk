a5dc5036c8ef65b4d33d590c76d6116de7ba7032
Mengqi Yu <mengqiy@google.com>
2021-06-30 17:59:20 -0700

    cleanup
    retire go SDK
    remove old gatekeeper function code

--- go/go.mod
+++ go/go.mod
@@ -3,20 +3,7 @@ module github.com/GoogleContainerTools/kpt-functions-sdk/go
 go 1.14
 
 require (
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/google/go-cmp v0.4.0
-	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/googleapis/gnostic v0.5.1 // indirect
-	github.com/open-policy-agent/frameworks/constraint v0.0.0-20200420221412-5bae2037a343
-	github.com/open-policy-agent/gatekeeper v3.0.4-beta.2+incompatible
-	github.com/pkg/errors v0.9.1
-	github.com/spf13/cobra v1.0.0
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
-	gopkg.in/yaml.v2 v2.4.0
+	github.com/kr/pretty v0.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
-	k8s.io/api v0.17.4
-	k8s.io/apimachinery v0.17.4
-	k8s.io/client-go v0.17.4 // indirect
-	k8s.io/utils v0.0.0-20200324210504-a9aa75ae1b89 // indirect
-	sigs.k8s.io/yaml v1.2.0 // indirect
 )
--- scripts/mirrorImages.sh
+++ scripts/mirrorImages.sh
@@ -5,15 +5,9 @@ set -euo pipefail
 declare -A imagearray=()
 imagearray["gcr.io/kpt-functions/read-yaml"]="gcr.io/config-management-release/read-yaml"
 imagearray["gcr.io/kpt-functions/write-yaml"]="gcr.io/config-management-release/write-yaml"
-imagearray["gcr.io/kpt-functions/gatekeeper-validate"]="gcr.io/kpt-functions/gatekeeper-validate"
-imagearray["gcr.io/kpt-functions/gatekeeper-validate"]="gcr.io/config-management-release/policy-controller-validate"
 imagearray["gcr.io/kpt-dev/kpt"]="gcr.io/config-management-release/kpt"
 imagearray["gcr.io/kpt-functions/helm-template"]="gcr.io/kpt-functions/helm-template"
 imagearray["gcr.io/kpt-functions/helm-template"]="gcr.io/config-management-release/helm-template"
-imagearray["gcr.io/kpt-functions/kubeval"]="gcr.io/kpt-functions/kubeval"
-imagearray["gcr.io/kpt-functions/kubeval"]="gcr.io/config-management-release/kubeval"
-imagearray["gcr.io/kpt-functions/istioctl-analyze"]="gcr.io/kpt-functions/istioctl-analyze"
-imagearray["gcr.io/kpt-functions/istioctl-analyze"]="gcr.io/config-management-release/istioctl-analyze"
 
 for image in "${!imagearray[@]}"; do
   docker pull ${image}
