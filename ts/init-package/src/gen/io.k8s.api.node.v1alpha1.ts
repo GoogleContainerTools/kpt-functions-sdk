fb98e96542de9d901bd647e7199a74a4dae3c3fb
Mengqi Yu <mengqiy@google.com>
2021-07-22 17:20:41 -0700

    update init-package

--- ts/init-package/src/gen/io.k8s.api.node.v1alpha1.ts
+++ ts/init-package/src/gen/io.k8s.api.node.v1alpha1.ts
@@ -21,28 +21,20 @@ export class RuntimeClass implements KubernetesObject {
   // APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
   public apiVersion: string;
 
-  // Handler specifies the underlying runtime and configuration that the CRI implementation will use to handle pods of this class. The possible values are specific to the node & CRI configuration.  It is assumed that all handlers are available on every node, and handlers of the same name are equivalent on every node. For example, a handler called "runc" might specify that the runc OCI runtime (using native Linux containers) will be used to run the containers in a pod. The Handler must be lowercase, conform to the DNS Label (RFC 1123) requirements, and is immutable.
-  public handler: string;
-
   // Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
   public kind: string;
 
   // More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
   public metadata: apisMetaV1.ObjectMeta;
 
-  // Overhead represents the resource overhead associated with running a pod for a given RuntimeClass. For more details, see https://git.k8s.io/enhancements/keps/sig-node/20190226-pod-overhead.md This field is alpha-level as of Kubernetes v1.15, and is only honored by servers that enable the PodOverhead feature.
-  public overhead?: Overhead;
-
-  // Scheduling holds the scheduling constraints to ensure that pods running with this RuntimeClass are scheduled to nodes that support it. If scheduling is nil, this RuntimeClass is assumed to be supported by all nodes.
-  public scheduling?: Scheduling;
+  // Specification of the RuntimeClass More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+  public spec: RuntimeClassSpec;
 
   constructor(desc: RuntimeClass.Interface) {
     this.apiVersion = RuntimeClass.apiVersion;
-    this.handler = desc.handler;
     this.kind = RuntimeClass.kind;
     this.metadata = desc.metadata;
-    this.overhead = desc.overhead;
-    this.scheduling = desc.scheduling;
+    this.spec = desc.spec;
   }
 }
 
@@ -51,24 +43,18 @@ export function isRuntimeClass(o: any): o is RuntimeClass {
 }
 
 export namespace RuntimeClass {
-  export const apiVersion = "node.k8s.io/v1beta1";
+  export const apiVersion = "node.k8s.io/v1alpha1";
   export const group = "node.k8s.io";
-  export const version = "v1beta1";
+  export const version = "v1alpha1";
   export const kind = "RuntimeClass";
 
   // RuntimeClass defines a class of container runtime supported in the cluster. The RuntimeClass is used to determine which container runtime is used to run all containers in a pod. RuntimeClasses are (currently) manually defined by a user or cluster provisioner, and referenced in the PodSpec. The Kubelet is responsible for resolving the RuntimeClassName reference before running the pod.  For more details, see https://git.k8s.io/enhancements/keps/sig-node/runtime-class.md
   export interface Interface {
-    // Handler specifies the underlying runtime and configuration that the CRI implementation will use to handle pods of this class. The possible values are specific to the node & CRI configuration.  It is assumed that all handlers are available on every node, and handlers of the same name are equivalent on every node. For example, a handler called "runc" might specify that the runc OCI runtime (using native Linux containers) will be used to run the containers in a pod. The Handler must be lowercase, conform to the DNS Label (RFC 1123) requirements, and is immutable.
-    handler: string;
-
     // More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
     metadata: apisMetaV1.ObjectMeta;
 
-    // Overhead represents the resource overhead associated with running a pod for a given RuntimeClass. For more details, see https://git.k8s.io/enhancements/keps/sig-node/20190226-pod-overhead.md This field is alpha-level as of Kubernetes v1.15, and is only honored by servers that enable the PodOverhead feature.
-    overhead?: Overhead;
-
-    // Scheduling holds the scheduling constraints to ensure that pods running with this RuntimeClass are scheduled to nodes that support it. If scheduling is nil, this RuntimeClass is assumed to be supported by all nodes.
-    scheduling?: Scheduling;
+    // Specification of the RuntimeClass More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+    spec: RuntimeClassSpec;
   }
 }
 
@@ -99,9 +85,9 @@ export function isRuntimeClassList(o: any): o is RuntimeClassList {
 }
 
 export namespace RuntimeClassList {
-  export const apiVersion = "node.k8s.io/v1beta1";
+  export const apiVersion = "node.k8s.io/v1alpha1";
   export const group = "node.k8s.io";
-  export const version = "v1beta1";
+  export const version = "v1alpha1";
   export const kind = "RuntimeClassList";
 
   // RuntimeClassList is a list of RuntimeClass objects.
@@ -111,6 +97,24 @@ export namespace RuntimeClassList {
 
     // Standard list metadata. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
     metadata?: apisMetaV1.ListMeta;
+  }
+}
+
+// RuntimeClassSpec is a specification of a RuntimeClass. It contains parameters that are required to describe the RuntimeClass to the Container Runtime Interface (CRI) implementation, as well as any other components that need to understand how the pod will be run. The RuntimeClassSpec is immutable.
+export class RuntimeClassSpec {
+  // Overhead represents the resource overhead associated with running a pod for a given RuntimeClass. For more details, see https://git.k8s.io/enhancements/keps/sig-node/20190226-pod-overhead.md This field is alpha-level as of Kubernetes v1.15, and is only honored by servers that enable the PodOverhead feature.
+  public overhead?: Overhead;
+
+  // RuntimeHandler specifies the underlying runtime and configuration that the CRI implementation will use to handle pods of this class. The possible values are specific to the node & CRI configuration.  It is assumed that all handlers are available on every node, and handlers of the same name are equivalent on every node. For example, a handler called "runc" might specify that the runc OCI runtime (using native Linux containers) will be used to run the containers in a pod. The RuntimeHandler must be lowercase, conform to the DNS Label (RFC 1123) requirements, and is immutable.
+  public runtimeHandler: string;
+
+  // Scheduling holds the scheduling constraints to ensure that pods running with this RuntimeClass are scheduled to nodes that support it. If scheduling is nil, this RuntimeClass is assumed to be supported by all nodes.
+  public scheduling?: Scheduling;
+
+  constructor(desc: RuntimeClassSpec) {
+    this.overhead = desc.overhead;
+    this.runtimeHandler = desc.runtimeHandler;
+    this.scheduling = desc.scheduling;
   }
 }
 
