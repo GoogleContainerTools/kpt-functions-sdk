295c6ed0d3c1e44f254c4f4b15d82e38599d98c7
Mengqi Yu <mengqiy@google.com>
2021-08-09 16:19:46 -0700

    copy type definitions for Kptfile and a helper function

---
+++ go/pkg/api/util/kptfileutil_test.go
@@ -0,0 +1,118 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	kptfilev1 "github.com/GoogleContainerTools/kpt-functions-sdk/go/pkg/api/kptfile/v1"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+func TestReadKptfile(t *testing.T) {
+	type testcase struct {
+		name        string
+		input       string
+		expected    *kptfilev1.KptFile
+		expectedErr string
+	}
+
+	testcases := []testcase{
+		{
+			name: "valid v1 Kptfile",
+			input: `apiVersion: kpt.dev/v1
+kind: Kptfile
+metadata:
+  name: test
+pipeline:
+  mutators:
+    - image: gcr.io/kpt-fn/starlark:unstable
+      configPath: fn-config.yaml
+`,
+			expected: &kptfilev1.KptFile{
+				ResourceMeta: yaml.ResourceMeta{
+					TypeMeta: yaml.TypeMeta{
+						APIVersion: "kpt.dev/v1",
+						Kind:       "Kptfile",
+					},
+					ObjectMeta: yaml.ObjectMeta{
+						NameMeta: yaml.NameMeta{
+							Name: "test",
+						},
+					},
+				},
+				Pipeline: &kptfilev1.Pipeline{
+					Mutators: []kptfilev1.Function{
+						{
+							Image:      "gcr.io/kpt-fn/starlark:unstable",
+							ConfigPath: "fn-config.yaml",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "invalid v1 Kptfile",
+			input: `apiVersion: kpt.dev/v1alpha1
+kind: Kptfile
+metadata:
+  name: nginx
+packageMetadata:
+  shortDescription: describe this package
+upstream:
+  type: git
+  git:
+    commit: 4d2aa98b45ddee4b5fa45fbca16f2ff887de9efb
+    repo: https://github.com/GoogleContainerTools/kpt
+    directory: package-examples/nginx
+    ref: v0.2
+openAPI:
+  definitions:
+    io.k8s.cli.setters.name:
+      x-k8s-cli:
+        setter:
+          name: name
+          value: the-map
+`,
+			expectedErr: "invalid 'v1' Kptfile",
+		},
+	}
+	runTestcase := func(tc testcase) (*kptfilev1.KptFile, error) {
+		rnode, err := yaml.Parse(tc.input)
+		if err != nil {
+			return nil, err
+		}
+		return ReadKptfile(rnode)
+	}
+	for _, tc := range testcases {
+		actual, err := runTestcase(tc)
+		if tc.expectedErr != "" {
+			if err == nil || !strings.Contains(err.Error(), tc.expectedErr) {
+				t.Errorf("%q is failing: expect error: %v to contain %q", tc.name, err, tc.expectedErr)
+				continue
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%q is failing: got unexpected error: %v", tc.name, err)
+				continue
+			}
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("%q is failing: expected: %+v, but got: %+v", tc.name, tc.expected, actual)
+			}
+		}
+	}
+}
