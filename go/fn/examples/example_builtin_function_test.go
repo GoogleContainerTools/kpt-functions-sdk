a70b561cb5936b8e7d964c3b10ad872be5aa93d0
natasha41575 <natashasarkar@google.com>
2022-03-22 14:18:28 -0700

    remove apply-replacements dependency

--- go/fn/examples/example_builtin_function_test.go
+++ go/fn/examples/example_builtin_function_test.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/GoogleContainerTools/kpt-functions-catalog/functions/go/apply-replacements/replacements"
 	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
 )
 
-// This example uses apply-replacements as a builtin function by using fn.Execute.
+// This example removes all resources as a builtin function by using fn.Execute.
+
+type RemoveAllResources struct{}
+
+func (*RemoveAllResources) Process(rl *fn.ResourceList) error {
+	rl.Items = nil
+	return nil
+}
 
 func Example_builtinFunction() {
 	reader := strings.NewReader(`
@@ -18,24 +24,18 @@ kind: ResourceList
 items:
 - kind: Deployment
   metadata:
-    name: source
+    name: my-deploy
 - kind: Service
   metadata:
-    name: target
+    name: my-service
 functionConfig:
   apiVersion: fn.kpt.dev/v1alpha1
-  kind: ApplyReplacements
+  kind: RemoveAllResources
   metadata:
-    name: replacements-fn-config
-  replacements:
-  - source: 
-      kind: Deployment
-    targets:
-    - select:
-        kind: Service`)
+    name: fn-config`)
 
 	var writer bytes.Buffer
-	err := fn.Execute(&replacements.Replacements{}, reader, &writer)
+	err := fn.Execute(&RemoveAllResources{}, reader, &writer)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -44,23 +44,11 @@ functionConfig:
 	// Output:
 	// apiVersion: config.kubernetes.io/v1
 	// kind: ResourceList
-	// items:
-	// - kind: Deployment
-	//   metadata:
-	//     name: source
-	// - kind: Service
-	//   metadata:
-	//     name: source
+	// items: []
 	// functionConfig:
 	//   apiVersion: fn.kpt.dev/v1alpha1
-	//   kind: ApplyReplacements
+	//   kind: RemoveAllResources
 	//   metadata:
-	//     name: replacements-fn-config
-	//   replacements:
-	//   - source:
-	//       kind: Deployment
-	//     targets:
-	//     - select:
-	//         kind: Service
+	//     name: fn-config
 	//
 }
--- go/fn/go.mod
+++ go/fn/go.mod
@@ -3,7 +3,6 @@ module github.com/GoogleContainerTools/kpt-functions-sdk/go/fn
 go 1.17
 
 require (
-	github.com/GoogleContainerTools/kpt-functions-catalog/functions/go/apply-replacements v0.0.0-20220316042125-d3eb289008bc
 	github.com/stretchr/testify v1.7.0
 	k8s.io/api v0.23.4
 	k8s.io/apimachinery v0.23.4
@@ -39,7 +38,5 @@ require (
 	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
 	k8s.io/utils v0.0.0-20211116205334-6203023598ed // indirect
 	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
-	sigs.k8s.io/kustomize/api v0.10.1 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
-	sigs.k8s.io/yaml v1.2.0 // indirect
 )
