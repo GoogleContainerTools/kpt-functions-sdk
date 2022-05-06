cdc9f0cfea190d80397dc05e2b514b2ba1c89b6c
Mengqi Yu <mengqiy@google.com>
2022-05-06 09:31:38 -0700

    parse results in resourceList (#571)

--- go/fn/resourcelist_test.go
+++ go/fn/resourcelist_test.go
@@ -1,6 +1,10 @@
 package fn
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 var dupResourceInput = []byte(`
 apiVersion: config.kubernetes.io/v1
@@ -24,6 +28,39 @@ func TestCheckResourceDuplication(t *testing.T) {
 	}
 	expectErr := "duplicate Resource(apiVersion=v1, kind=Namespace, Namespace=, Name=example)"
 	if err.Error() != expectErr {
-		t.Errorf("expect CheckResourceDuplication to fail; got %v, want %v", err ,expectErr)
+		t.Errorf("expect CheckResourceDuplication to fail; got %v, want %v", err, expectErr)
+	}
+}
+
+func TestParseResourceListResults(t *testing.T) {
+	rl, err := ParseResourceList([]byte(`
+apiVersion: config.kubernetes.io/v1
+kind: ResourceList
+items:
+- apiVersion: v1
+  kind: Namespace
+  metadata:
+    name: example
+results:
+- message: foo
+  severity: error
+- message: bar
+  severity: warning
+`))
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	expected := Results{
+		{
+			Message:  "foo",
+			Severity: Error,
+		},
+		{
+			Message:  "bar",
+			Severity: Warning,
+		},
+	}
+	if !cmp.Equal(expected, rl.Results) {
+		t.Fatalf("unexpected diff: %v", cmp.Diff(expected, rl.Results))
 	}
 }
