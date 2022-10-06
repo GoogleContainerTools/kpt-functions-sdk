195fb21bbb17de0fa57a76cc453236771e6d3c9d
Yuwen Ma <yuwenma@google.com>
2022-10-06 16:43:16 -0700

    fix fnRunner example

--- go/fn/examples/example_asmain_runner_test.go
+++ go/fn/examples/example_asmain_runner_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,14 +29,16 @@ type SetLabels struct {
 // `ctx` provides easy methods to add info, error or warning result to `ResourceList.Results`.
 // `items` is parsed from the STDIN "ResourceList.Items".
 // `functionConfig` is from the STDIN "ResourceList.FunctionConfig". The value has been assigned to the r.Labels
-//  the functionConfig is validated to have kind "SetLabels" and apiVersion "fn.kpt.dev/v1alpha1"
-func (r *SetLabels) Run(ctx *fn.Context, functionConfig *fn.KubeObject, items fn.KubeObjects) {
+//
+//	the functionConfig is validated to have kind "SetLabels" and apiVersion "fn.kpt.dev/v1alpha1"
+func (r *SetLabels) Run(_ *fn.Context, _ *fn.KubeObject, items fn.KubeObjects, results *fn.Results) bool {
 	for _, o := range items {
 		for k, newLabel := range r.Labels {
 			o.SetLabel(k, newLabel)
 		}
 	}
-	ctx.ResultInfo("updated labels", nil)
+	results.Infof("updated labels")
+	return true
 }
 
 // This example uses a SetLabels object, which implements `Runner.Run` methods.
@@ -48,12 +50,14 @@ func (r *SetLabels) Run(ctx *fn.Context, functionConfig *fn.KubeObject, items fn
 //   - apiVersion: v1
 //     kind: Service
 //     metadata:
-//       name: example
+//     name: example
+//
 // functionConfig:
-//   apiVersion: fn.kpt.dev/v1alpha1
-//   kind: SetLabels
-//   metadata:
-//     name: setlabel-fn-config
+//
+//	apiVersion: fn.kpt.dev/v1alpha1
+//	kind: SetLabels
+//	metadata:
+//	  name: setlabel-fn-config
 func Example_asMain() {
 	file, _ := os.Open("./data/setlabels-resourcelist.yaml")
 	defer file.Close()
