cd1ef4b82a115c5d1047e1446f400625605b8ea4
Yuwen Ma <yuwenma@google.com>
2022-10-07 11:21:55 -0700

    fix fnRunner example

--- go/fn/examples/example_asmain_runner_test.go
+++ go/fn/examples/example_asmain_runner_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -50,13 +50,14 @@ func (r *SetLabels) Run(ctx *fn.Context, functionConfig *fn.KubeObject, items fn
 //   - apiVersion: v1
 //     kind: Service
 //     metadata:
-//       name: example
+//     name: example
+//
 // functionConfig:
-//   apiVersion: fn.kpt.dev/v1alpha1
-//   kind: SetLabels
-//   metadata:
-//     name: setlabel-fn-config
-func main() {
+//	apiVersion: fn.kpt.dev/v1alpha1
+//	kind: SetLabels
+//	metadata:
+//	  name: setlabel-fn-config
+func Example_asMain() {
 	file, _ := os.Open("./data/setlabels-resourcelist.yaml")
 	defer file.Close()
 	os.Stdin = file
